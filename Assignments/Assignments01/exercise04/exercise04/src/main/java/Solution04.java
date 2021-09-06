afb0ab0116d7aa1550cfe07800d5a5e66f4f90b8
hunter chang <aqua577@outlook.com>
2021-09-05 22:57:51 -0400

    exercise04

---
+++ Assignments/Assignments01/exercise04/exercise04/src/main/java/Solution04.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Solution04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);  // Creates a scanner
+        System.out.println("Enter a noun: ");
+        String noun = input.nextLine();  // Reads user input
+
+        System.out.println("Enter a verb: ");
+        String verb = input.nextLine();  // Reads user input
+
+        System.out.println("Enter a adjective: ");
+        String adjective = input.nextLine();  // Reads user input
+
+        System.out.println("Enter a adverb: ");
+        String adverb = input.nextLine();  // Reads user input
+
+        input.close();
+
+        System.out.println(noun + " likes to " + verb + " " + adverb + " to his favorite " + adjective + " pond yearly.");  // Print line with inputs (Mad Lib)
+    }
+}
