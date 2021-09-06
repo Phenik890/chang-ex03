7eaa0bab8373ae1ea7caf0fe3d92c06353527407
hunter chang <aqua577@outlook.com>
2021-09-05 23:24:53 -0400

    exercise03 commit

--- Assignments/Assignments01/exercise03/exercise03/src/main/java/Solution03.java
+++ Assignments/Assignments01/exercise03/exercise03/src/main/java/Solution03.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Hunter Chang
+ */
+
 public class Solution03 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);  // Creates a scanner
